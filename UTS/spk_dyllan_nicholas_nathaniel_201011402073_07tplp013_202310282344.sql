06979f36f4d655f0dfc727eb00d18b2a27aa9904
D3Xe <114375108+D3Xect@users.noreply.github.com>
2023-10-28 23:48:28 +0700

    Add files via upload

---
+++ UTS/spk_dyllan_nicholas_nathaniel_201011402073_07tplp013_202310282344.sql
@@ -0,0 +1,11 @@
+INSERT INTO "Dyllan".spk_dyllan_nicholas_nathaniel_201011402073_07tplp013 ("no",nama,ram,memori_penyimpanan,processor,baterai,ukuran_layar,harga_indonesia) VALUES
+	 (1,'Xiaomi Redmi Note 11 Pro 5G','6GB','128GB','MediaTek Dimensity 920 | (4 core) | 2.5 GHz','5.000 mAh','6,67 inci','Rp5.799.000'),
+	 (2,'Xiaomi Redmi Note 11 Pro','6GB','128GB','MediaTek Helio G96 | (6 core) | 2.0 GHz','5.000 mAh','6,67 inci','Rp4.299.000'),
+	 (3,'Xiaomi Redmi Note 11S','6GB','128GB','MediaTek Helio G96 | (6 core) | 2.0 GHz','5.000 mAh','6,43 inci','Rp3.599.000'),
+	 (4,'Xiaomi Redmi Note 11','4GB','64GB','MediaTek Helio G88 | (6 core) | 1.8 GHz','5.000 mAh','6,43 inci','Rp2.999.000'),
+	 (5,'Xiaomi Redmi 10C','4GB','64GB','MediaTek Helio G35 | (4 core) | 2.3 GHz','5.000 mAh','6,71 inci','Rp1.999.000'),
+	 (6,'Xiaomi Redmi 9C','3GB','64GB','MediaTek Helio G35 | (4 core) | 2.3 GHz','5.000 mAh','6,53 inci','Rp1.699.000'),
+	 (7,'Xiaomi Redmi 9A','2GB','32GB','MediaTek Helio G25 | (4 core) | 2.0 GHz','5.000 mAh','6,53 inci','Rp1.299.000'),
+	 (8,'Xiaomi Redmi 9','3GB','32GB','MediaTek Helio G35 | (4 core) | 2.3 GHz','5.000 mAh','6,53 inci','Rp1.499.000'),
+	 (9,'Xiaomi Poco M4 Pro 5G','4GB','64GB','MediaTek Dimensity 810 | (6 core) | 2.0 GHz','5.000 mAh','6,6 inci','Rp2.199.000'),
+	 (10,'Xiaomi Poco M3 Pro 5G','4GB','64GB','MediaTek Dimensity 700 | (6 core) | 2.0 GHz','5.000 mAh','6,5 inci','Rp2.299.000');
